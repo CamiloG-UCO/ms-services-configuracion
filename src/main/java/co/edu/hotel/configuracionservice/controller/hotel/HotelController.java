a384000d6916fd7acd4e8c5fb15b32234dad634d
Andrés Cardona <andres.cardo98@gmail.com>
2025-11-06 14:08:28 -0500

    CrossOrigin in HotelController

--- src/main/java/co/edu/hotel/configuracionservice/controller/hotel/HotelController.java
+++ src/main/java/co/edu/hotel/configuracionservice/controller/hotel/HotelController.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/hotel")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HotelController {
 
     private final HotelService hotelService;
